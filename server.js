c29a5ae2826845c0b6b35777d7205ef5076f859b
Max Severin <contact@max-severin.com>
2021-05-21 19:21:39 +0000

    Add mongoDB connection and mongoose

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
 
-//;
+//
+
+mongoose.connect(
+  process.env.MONGO_URI, 
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  }
+);
+
+mongoose.connection.on(
+  'error', 
+  console.error.bind(console, 'connection error:')
+);
+
+mongoose.connection.once('open', () => {
+	console.log('MongoDB database connection established successfully');
+});
+
+//
 
 app.use(cors());
 
