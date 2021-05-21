ea50e46f86f0961b2ce3279407ba8fc021ec919c
Max Severin <contact@max-severin.com>
2021-05-21 22:00:51 +0000

    Add /api/shorturl POST endpoint

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const validUrl = require('valid-url');
+const shortId = require('shortid');
 
 const app = express();
 const port = 3000;
@@ -13,7 +16,7 @@ mongoose.connect(
   process.env.MONGO_URI, 
   { 
     useNewUrlParser: true, 
-    useUnifiedTopology: true 
+    useUnifiedTopology: true,
   }
 );
 
@@ -30,25 +33,65 @@ const { Schema } = mongoose;
 
 const urlSchema = new Schema({
   originalUrl: { type: String, required: true },
-  shortUrl: { type: String, required: true }
+  shortUrl: { type: String, required: true },
 });
 
-const URL = mongoose.model('URL', urlSchema);
+const urlModel = mongoose.model('URL', urlSchema);
 
 //
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
-  next();
-});
-
 app.use('/public', express.static(`${__dirname}/public`));
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 //
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/view/index.html`));
+
+app.post('/api/shorturl', async (req, res) => {
+  const url = req.body.url.trim();
+
+  if (!validUrl.isUri(url)) {
+    res.status(404).json({
+      message: "Invalid URL" 
+    });
+  } else {    
+    try {
+      const existedOne = await urlModel.findOne({
+        originalUrl: url
+      });
+
+      if (existedOne) {       
+        res.json({ 
+          originalUrl: existedOne.originalUrl,
+          shortUrl: existedOne.shortUrl,
+        });
+      } else {
+        const shortUrl = shortId.generate();
+
+        const newOne = new urlModel({
+          originalUrl: url,
+          shortUrl: shortUrl,
+        });
+
+        await newOne.save();
+
+        res.json({ 
+          originalUrl: newOne.originalUrl,
+          shortUrl: newOne.shortUrl,
+        });
+      }
+
+    } catch(error) {
+      res.status(500).json({
+        error,
+        message: "Server Error",
+      });
+    }      
+  }  
+});
 
 //
 
